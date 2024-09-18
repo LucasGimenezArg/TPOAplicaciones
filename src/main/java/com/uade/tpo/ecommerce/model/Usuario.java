f78f0647e10ab2417524ec9de82ecf12bbe0522d
Leonardo Rodriguez <leonardo.rodriguez@mulesoft.com>
2024-09-17 22:18:00 -0300

    Security first version

--- src/main/java/com/uade/tpo/ecommerce/model/Usuario.java
+++ src/main/java/com/uade/tpo/ecommerce/model/Usuario.java
@@ -1,13 +1,20 @@
 package com.uade.tpo.ecommerce.model;
 
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
 import java.util.Date;
+import java.util.List;
 
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "tipo_usuario", discriminatorType = DiscriminatorType.STRING)
-public abstract class Usuario {
+public abstract class Usuario implements UserDetails {
+    public static final String ROL_ADMIN = "admin";
+    public static final String ROL_NORMAL = "normal";
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -70,5 +77,22 @@ public abstract class Usuario {
     // Getter para id
     public long getId() {
         return id;
+    }
+
+    public abstract String getRol();
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(getRol()));
+    }
+
+    @Override
+    public String getPassword() {
+        return this.getContrasena();
+    }
+
+    @Override
+    public String getUsername() {
+        return this.getMail();
     }
 }
--- src/main/java/com/uade/tpo/ecommerce/model/UsuarioNormal.java
+++ src/main/java/com/uade/tpo/ecommerce/model/UsuarioNormal.java
@@ -1,8 +1,11 @@
 package com.uade.tpo.ecommerce.model;
 
+import java.util.Collection;
 import java.util.Date;
+import java.util.List;
 
 import jakarta.persistence.Entity;
+import org.springframework.security.core.GrantedAuthority;
 
 @Entity
 public class UsuarioNormal extends Usuario {
@@ -12,5 +15,10 @@ public class UsuarioNormal extends Usuario {
 
     public UsuarioNormal() {
 
+    }
+
+    @Override
+    public String getRol() {
+        return ROL_NORMAL;
     }
 }
